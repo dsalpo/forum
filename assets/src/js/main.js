3c05691cb5b42098cd564fe4d6696089b1088e8d
Davlat <davlat_sa_2001@mail.ru>
2018-07-04 09:13:53 -0700

    added header

---
+++ assets/src/js/main.js
@@ -0,0 +1,6 @@
+// Импортируем jQuery
+//= ../../../bower_components/jquery/dist/jquery.js
+
+//= ../../../bower_components/bootstrap-sass/assets/javascripts/bootstrap.min.js
+
+//= ../../../bower_components/slick-carousel/slick/slick.min.js
